84f5c35be2d979d79aca2c8aa6baedc0fe086bec
Kasper Laursen <habberlabber@gmail.com>
2021-04-07 20:40:04 +0200

    Cleanup rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,9 @@
 import svelte from "rollup-plugin-svelte";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
-import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
 import sveltePreprocess from "svelte-preprocess";
 import typescript from "@rollup/plugin-typescript";
-import css from "rollup-plugin-css-only";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -73,10 +71,6 @@ export default {
     // In dev mode, call `npm run start` once
     // the bundle has been generated
     !production && serve(),
-
-    // Watch the `public` directory and refresh the
-    // browser on changes when not in production
-    // !production && livereload("public"),
 
     // If we're building for production (npm run build
     // instead of npm run dev), minify

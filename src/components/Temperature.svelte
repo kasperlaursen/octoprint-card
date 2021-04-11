cccff3f8d7f62374daaf63393dedb42f38889cc4
Kasper Laursen <habberlabber@gmail.com>
2021-04-11 17:38:39 +0200

    Create Temperature Component

---
+++ src/components/Temperature.svelte
@@ -0,0 +1,25 @@
+<svelte:options tag="octoprint-card-temperature" />
+
+<script lang="ts">
+  export let actual: { value: string; unit: string };
+  export let target: { value: string; unit: string };
+  export let label: string;
+  export let cssClass: string;
+</script>
+
+<div class={`temperature ${cssClass}`}>
+  {#if actual}
+    <b>
+      {actual?.value}{actual?.unit}
+      {#if target}
+        / {target?.value}{target?.unit}
+      {/if}
+    </b>
+  {:else}
+    <b>Unavailable</b>
+  {/if}
+
+  {#if label}
+    <span>{label}</span>
+  {/if}
+</div>
